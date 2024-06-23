e56f49821781f4ad5fb320d8d23b951191d0f788
Mbata Precious <mbataprecious9@gmail.com>
2024-06-23 10:17:34 +0100

    this is the code .

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             We utilize your camera image to ensure fairness for all
             participants, and we also employ both your camera and microphone for
             a video questions where you will be prompted to record a response
-            using your camera or webcam, so it's essential to verify that your
+            using your camera or webcam, so it&apos;s essential to verify that your
             camera and microphone are functioning correctly and that you have a
             stable internet connection. To do this, please position yourself in
             front of your camera, ensuring that your entire face is clearly
